47737288cd018d34fc397c1181cbbe6eca0bb9dd
helen-akateva <akateva17@gmail.com>
2025-08-26 13:01:15 +0300

    task 2

--- js/task-2.js
+++ js/task-2.js
@@ -9,11 +9,9 @@ class Storage {
   addItem(newItem) {
     this.#items.push(newItem);
   }
+
   removeItem(itemToRemove) {
-    const index = this.#items.indexOf(itemToRemove);
-    if (index > -1) {
-      this.#items.splice(index, 1);
-    }
+    this.#items = this.#items.filter(item => item !== itemToRemove);
   }
 }
 
